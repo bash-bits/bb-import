acf9b63a35ad762580e4d01a9a574b688271dc27
Ragdata <ragdata@users.noreply.github.com>
2023-04-17 18:54:27 +1000

    style(install): minor updates to installer

--- install.sh
+++ install.sh
@@ -389,7 +389,7 @@ install::menu()
 	echo "      3) Uninstall"
 	echo "      Q) Quit"
 	echo
-	echo "  Type your selection: (${GOLD}1${RESET}/2/3/Q) " -n
+	echoSuccess "  Type your selection: (${GOLD}1${RESET}/2/3/Q) " -n
 	echo
 	while [[ ! "$INST" =~ [123Qq] ]]
 	do
