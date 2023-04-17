43515bf41a5305ddb813ed5398c53694d37f718a
Ragdata <ragdata@users.noreply.github.com>
2023-04-17 19:21:41 +1000

    style(install): minor updates to installer

--- install.sh
+++ install.sh
@@ -16,7 +16,7 @@
 # set debug mode = false
 declare -gx DEBUG=true
 # if script is called with 'debug' as an argument, then set debug mode
-if [[ "${1,,}" == "debug" ]]; then shift; DEBUG=true; set -- "${@}"; set -axeET; else set -aeET; fi
+if [[ "$DEBUG" ]]; then set -axeET; else set -aeET; fi
 # ==================================================================
 # VARIABLES
 # ==================================================================
